b44a4cea9f5388035934644bf93c2e2f7d65b970
chenzeng765 <chenzeng765@gmail.com>
2022-06-21 14:47:43 +0800

    add webhook events (#7)
    * add converter.py to convert payload and init Payload class to avoid errors when using nested class to get payload parameters
    
    * format code
    
    Co-authored-by: wangwenliang <915840547@qq.com>

---
+++ gitee/models/hook_event_helper.py
@@ -0,0 +1,17 @@
+EVENT_TYPE_NOTE = "Note Hook"
+EVENT_TYPE_PUSH = "Push Hook"
+EVENT_TYPE_ISSUE = "Issue Hook"
+EVENT_TYPE_PR = "Merge Request Hook"
+# StatusOpen gitee issue or pr status is open
+STATUS_OPEN = "open"
+# StatusClosed gitee issue or pr status is closed
+STATUS_CLOSED = "closed"
+STATUS_MERGED = "merged"
+ACTION_OPEN = "open"
+ACTION_CLOSE = "close"
+PR_ACTION_MERGE = "merge"
+PR_ACTION_UPDATED_LABEL = "update_label"
+PR_ACTION_CHANGED_TARGET_BRANCH = "target_branch_changed"
+PR_ACTION_CHANGED_SOURCE_BRANCH = "source_branch_changed"
+PR_ACTION_LINK_ISSUE = "update_link_issue"
+ACTION_ADD_LABEL = "add_label"
---
+++ gitee/models/model_user_basic.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class UserBasic:
+    id: Optional[int] = 0
+    login: Optional[str] = ""
+    name: Optional[str] = ""
+    avatar_url: Optional[str] = ""
+    url: Optional[str] = ""
+    html_url: Optional[str] = ""
+    followers_url: Optional[str] = ""
+    following_url: Optional[str] = ""
+    gists_url: Optional[str] = ""
+    starred_url: Optional[str] = ""
+    subscriptions_url: Optional[str] = ""
+    organizations_url: Optional[str] = ""
+    repos_url: Optional[str] = ""
+    events_url: Optional[str] = ""
+    received_events_url: Optional[str] = ""
+    type: Optional[str] = ""
+    site_admin: Optional[bool] = False
+    email: Optional[str] = ""
